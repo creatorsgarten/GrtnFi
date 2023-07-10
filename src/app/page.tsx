d19d4656fd40f1ecf0c0ee53f508b2eee406847a
dtinth on MBP M1 <dtinth@spacet.me>
2023-07-10 18:32:39 +0700

    ‌

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,9 @@ export default function Home() {
       GrtnFi
       <hr />
       <h1>showdown.space</h1>
-      <AccountTable cashOnHandAccounts={['dtinth']} />
+      <AccountTable hostAccounts={['dtinth']} />
+      <h1>sht7</h1>
+      <EventTable eventSlug="sht7" />
     </main>
   )
 }
@@ -41,6 +43,8 @@ const RawTransactionRow = z.object({
   }),
 })
 
+type RawTransactionRow = z.infer<typeof RawTransactionRow>
+
 async function getTransactions() {
   const response = await fetch(
     'https://db.creatorsgarten.org/api/v1/db/data/v1/Creatorsgarten%20Operations/Transactions?limit=1000&shuffle=0&offset=0&nested%5BDebit%5D%5Bfields%5D=Id%2CTitle%2CType&nested%5BCredit%5D%5Bfields%5D=Id%2CTitle%2CType',
@@ -64,31 +68,100 @@ async function getTransactions() {
   return { list: parsed }
 }
 
-interface AccountTable {
-  cashOnHandAccounts: string[]
-}
-async function AccountTable(props: AccountTable) {
+async function getAccountTransactions(hostAccounts: string[]) {
   const data = await getTransactions()
-  const isCashOnHand = (account: { Title: string }) => {
-    return props.cashOnHandAccounts.includes(account.Title)
+  const isHostAccount = (account: { Title: string }) => {
+    return hostAccounts.includes(account.Title)
   }
-  const transactions = data.list
-    .filter((row) => {
-      return isCashOnHand(row.Debit) || isCashOnHand(row.Credit)
-    })
-    .map((row) => ({
+  return toTable(
+    data.list.filter((row) => {
+      return isHostAccount(row.Debit) || isHostAccount(row.Credit)
+    }),
+  )
+}
+async function getEventTransactions(slug: string) {
+  const data = await getTransactions()
+  return toTable(
+    data.list.filter((row) => {
+      return row.Event?.Title === slug
+    }),
+  )
+}
+
+function toTable(list: RawTransactionRow[]): TransactionTableRow[] {
+  const isCashOnHand = (account: { Type: string }) => {
+    return account.Type === 'Cash on Hand'
+  }
+  const isReceivable = (account: { Type: string }) => {
+    return account.Type === 'Receivables'
+  }
+  let balance = 0
+  return list.map((row) => {
+    const amount = isCashOnHand(row.Debit)
+      ? row.Amount
+      : isCashOnHand(row.Credit)
+      ? -row.Amount
+      : 0
+    const received = isCashOnHand(row.Debit)
+    const receiving = isReceivable(row.Debit)
+    const { account, accountType } = (() => {
+      const use = (a: { Title: string; Type: string }) => {
+        return {
+          account: a.Title,
+          accountType: a.Type,
+        }
+      }
+      if (received || receiving) {
+        return use(row.Credit)
+      } else {
+        return use(row.Debit)
+      }
+    })()
+    return {
       id: row.Id,
       date: row.Date,
       notes: row.Notes,
       description: row.Title,
-      amount: isCashOnHand(row.Debit) ? row.Amount : -row.Amount,
-      account: isCashOnHand(row.Debit) ? row.Credit.Title : row.Debit.Title,
-      accountType: isCashOnHand(row.Debit) ? row.Credit.Type : row.Debit.Type,
+      amount: amount,
+      balance: (balance += amount),
       event: row.Event?.Title,
-    }))
+      account,
+      accountType,
+    }
+  })
+}
+interface TransactionTableRow {
+  id: number
+  date: string
+  notes?: string | null
+  description: string
+  amount: number
+  balance: number
+  account: string
+  accountType: string
+  event?: string | null
+}
 
-  let balance = 0
+interface AccountTable {
+  hostAccounts: string[]
+}
+async function AccountTable(props: AccountTable) {
+  const transactions = await getAccountTransactions(props.hostAccounts)
+  return <TransactionTable transactions={transactions} />
+}
 
+interface EventTable {
+  eventSlug: string
+}
+async function EventTable(props: EventTable) {
+  const transactions = await getEventTransactions(props.eventSlug)
+  return <TransactionTable transactions={transactions} />
+}
+
+interface TransactionTable {
+  transactions: TransactionTableRow[]
+}
+function TransactionTable(props: TransactionTable) {
   return (
     <>
       <table className="table">
@@ -97,7 +170,7 @@ async function AccountTable(props: AccountTable) {
             <th scope="col">Date</th>
             <th scope="col">Description</th>
             <th scope="col" className="text-end">
-              Out / In
+              Amount
             </th>
             <th scope="col" className="text-end">
               Balance
@@ -105,11 +178,10 @@ async function AccountTable(props: AccountTable) {
           </tr>
         </thead>
         <tbody>
-          {transactions.map((row) => {
-            balance += row.amount
+          {props.transactions.map((row) => {
             return (
               <tr key={row.id}>
-                <td>
+                <td style={{ whiteSpace: 'nowrap' }}>
                   {row.date}
                   {!!row.event && (
                     <>
@@ -129,28 +201,31 @@ async function AccountTable(props: AccountTable) {
                   </small>
                 </td>
                 <td
-                  className={'text-end'}
+                  className={
+                    'text-end ' +
+                    (row.amount < 0 ? 'text-danger' : 'text-success')
+                  }
                   style={{
                     fontVariantNumeric: 'tabular-nums',
-                    paddingRight: row.amount < 0 ? '5em' : '',
+                    whiteSpace: 'nowrap',
                   }}
                 >
                   {formatMoney(row.amount)}
                 </td>
                 <td
                   className="text-end"
-                  style={{ fontVariantNumeric: 'tabular-nums' }}
+                  style={{
+                    fontVariantNumeric: 'tabular-nums',
+                    whiteSpace: 'nowrap',
+                  }}
                 >
-                  {formatMoney(balance)}
+                  {formatMoney(row.balance)}
                 </td>
               </tr>
             )
           })}
         </tbody>
       </table>
-      {/* <pre style={{ whiteSpace: 'pre-wrap' }}>
-        {JSON.stringify(data, null, 2)}
-      </pre> */}
     </>
   )
 }
